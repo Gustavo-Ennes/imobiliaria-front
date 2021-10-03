9419605d59582aca7994a8118dbc1648c3250968
Gustavo-Ennes <gustavo_ennes@yahoo.com>
2021-10-03 17:49:15 -0300

    page folder structure

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,25 +4,25 @@ const routes = [
     path: '/admin/',
     component: () => import('layouts/AdminLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/AdminPage.vue') },
-      { path: 'tenants/', component: () => import('pages/AdminTenantsPage.vue') },
-      { path: 'owners/', component: () => import('pages/AdminOwnersPage.vue') },
-      { path: 'lands/', component: () => import('pages/AdminLandsPage.vue') },
-      { path: 'properties/', component: () => import('pages/AdminPropertiesPage.vue') }
+      { path: '', component: () => import('src/pages/admin/AdminPage.vue') },
+      { path: 'tenants/', component: () => import('src/pages/admin/AdminTenantsPage.vue') },
+      { path: 'owners/', component: () => import('src/pages/admin/AdminOwnersPage.vue') },
+      { path: 'lands/', component: () => import('src/pages/admin/AdminLandsPage.vue') },
+      { path: 'properties/', component: () => import('src/pages/admin/AdminPropertiesPage.vue') }
     ]
   },
   {
     path: '/tenant/',
     component: () => import('layouts/TenantLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/TenantPage.vue') }
+      { path: '', component: () => import('src/pages/tenant/TenantPage.vue') }
     ]
   },
   {
     path: '/owner/',
     component: () => import('layouts/OwnerLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/OwnerPage.vue') }
+      { path: '', component: () => import('src/pages/owner/OwnerPage.vue') }
     ]
   },
 
